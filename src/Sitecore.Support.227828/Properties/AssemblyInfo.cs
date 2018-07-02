89b25a15778d087d3d6a58fa5ce435a887bf4711
Paul Kravchenko <pk@sitecore.net>
2018-07-02 16:00:46 +0300

    Initial project commit

---
+++ src/Sitecore.Support.227828/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.227828")]
+[assembly: AssemblyProduct("Sitecore.Support.227828")]
+[assembly: ComVisible(false)]
